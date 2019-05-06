66a03c5ded1a58c3af8736a4d26a9a75befdc4e4
wcarllWRLD <43218005+wcarllWRLD@users.noreply.github.com>
2019-05-06 14:11:22 -0500

    worked on map design

--- tutorial.py
+++ tutorial.py
@@ -15,47 +15,80 @@ green = (0,255,0)
 blue = (0,0,255)
 grass = pygame.image.load('grass.png')
 road = pygame.image.load('road.png')
+finish = pygame.image.load('finish_line.png')
 gameDisplay = pygame.display.set_mode((dis_width,dis_height))
 class Tile:
 
-    def __init__(self,slow_down):
+    def __init__(self,slow_down,finish_line):
         self.slow_down = slow_down
+        self.finish_line = finish_line
 
 def create_map():
-    mymap = [[ Tile(False) for x in range(0,map_width)] for y in range(0,map_height)]
+    mymap = [[ Tile(False,False) for x in range(0,map_width)] for y in range(0,map_height)]
     #map2 = [[ Tile(False) for x in range(0,map_width)] for y in range(0,map_height)]
 
 #mymap = [[r*30],[r*30]]
 
 #mymap[10][10].slow_down = True
-    for y in range(10,36):
-        for x in range(10,51):
+    for y in range(12,33):
+        for x in range(15,46):
             mymap[y][x].slow_down = True
+    for i in range(60):
+        mymap[0][i].slow_down = True
+        mymap[44][i].slow_down = True
+        if i < 45:
+            mymap[i][0].slow_down = True
+            mymap[i][59].slow_down = True
 
-    mymap[0][0].slow_down = True
     #top left
-    mymap[44][59].slow_down = True
+    for i in range(6):
+        mymap[44-i][59].slow_down = True
+        mymap[0][i].slow_down = True
+        mymap[i][0].slow_down = True
+        if i < 5:
+            mymap[44-i][58].slow_down = True
+            mymap[1][i].slow_down = True
+            mymap[i][1].slow_down = True
+        if i < 4:
+            mymap[44-i][57].slow_down = True
+            mymap[2][i].slow_down = True
+            mymap[i][2].slow_down = True
+        if i < 3:
+            mymap[44-i][56].slow_down = True
+            mymap[3][i].slow_down = True
+            mymap[i][3].slow_down = True
+        if i < 2:
+            mymap[44-i][55].slow_down = True
+            mymap[4][i].slow_down = True
+            mymap[i][3].slow_down = True
+    
+    
+    
+    
     #bottom right
     mymap[44][0].slow_down = True
     #bottom left
     mymap[0][59].slow_down = True
+    mymap[5][30].finish_line = True
     #top right
     return mymap
-
-
-
 #surface main^
 def draw_map(m):
     for x in range(map_width):
         for y in range(map_height):
+            if m[y][x].slow_down == False:
+                #draw road
+                gameDisplay.blit(road, (x*cell_width, y* cell_height))
             if m[y][x].slow_down:
                 #draw grass
                 gameDisplay.blit(grass, (x*cell_width, y* cell_height))
 
+            if m[y][x].finish_line:
+                #draw finish line
+                gameDisplay.blit(finish, (x*cell_width, y* cell_height))
+
                 
-            else:
-                #draw road
-                gameDisplay.blit(road, (x*cell_width, y* cell_height))
+    
                 
 
 
