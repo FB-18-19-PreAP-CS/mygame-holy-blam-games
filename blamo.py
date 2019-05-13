3d957824fad58a2d556896c2a87827a6f0e32a43
Logan <logany2013@gmai.com>
2019-05-13 13:24:51 -0500

    changes

--- blamo.py
+++ blamo.py
@@ -22,61 +22,68 @@ def cars():
     angle_dos = 0
     while not done:
         for event in pygame.event.get():
-
                 if event.type == pygame.QUIT:
                         done = True
         my_map = create_map()
-        draw_map(my_map)
+        # draw_map(my_map)
         showSprite(viper)
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_c]: speed-=.25
+        if pressed[pygame.K_m]: speed-=.25
         if pressed[pygame.K_UP]:
-            speed+=.01
+            speed+=.5
             x +=speed* math.cos(angle*math.pi/180)
             y += speed* math.sin(angle*math.pi/180)
         if pressed[pygame.K_DOWN]:
             y -=speed* math.sin(angle*math.pi/180)
             x -=speed* math.cos(angle*math.pi/180)
         if pressed[pygame.K_LEFT]: 
-            angle += -4
+            angle += -7
             transformSprite(viper, angle+90, .1)
         if pressed[pygame.K_RIGHT]: 
-            angle+= 4
+            angle+= 7
             transformSprite(viper, angle+90, .1)
 
-        if x > 900:
-            x = 880
+        if x > 1200:
+            x = 1100
+            speed-=10
         if x < 0:
             x = 20
-        if y > 600:
-            y= 580
+            speed-=10
+        if y > 900:
+            y= 880
+            speed-=10
         if y < 0:
             y= 20
+            speed-=10
         moveSprite(viper,x,y)
         showSprite(popo)
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_r]: speed_2-=.25
         if pressed[pygame.K_w]:
-            speed_2+=.01
+            speed_2+=.5
             o +=speed_2* math.cos(angle_dos*math.pi/180)
             p += speed_2* math.sin(angle_dos*math.pi/180)
         if pressed[pygame.K_s]:
             o -=speed_2* math.sin(angle_dos*math.pi/180)
             p -=speed_2* math.cos(angle_dos*math.pi/180)
         if pressed[pygame.K_a]: 
-            angle_dos+= -4
+            angle_dos+= -7
             transformSprite(popo, angle_dos+90, .1)
         if pressed[pygame.K_d]: 
-            angle_dos+= 4
+            angle_dos+= 7
             transformSprite(popo, angle_dos+90, .1)
-        if o > 900:
-            o = 880
+        if o > 1200:
+            o = 1100
+            speed_2-=10
         if o < 0:
             o = 20
-        if p > 600:
-            p= 580
+            speed_2-=10
+        if p > 900:
+            p= 880
+            speed_2-=10
         if p < 0:
             p= 20
+            speed_2-=10
         moveSprite(popo,o,p)
         angle_dos = angle_dos%360
         angle = angle%360
@@ -91,8 +98,17 @@ def cars():
             speed = 1 
         if speed_2 < 0:
             speed_2 = 1
-        speed = speed%20
-        speed_2 = speed_2%20
+        if speed > 20:
+            speed = 20
+        if speed_2 > 20:
+            speed_2 = 20
+        if my_map[int(y//20)][int(x//20)].slow_down:
+            speed-=1 
+        if my_map[int(p//20)][int(o//20)].slow_down:
+            speed_2-=1
+
+
+        
 
 
         # if is_blue: color = (0, 128, 255)
@@ -105,7 +121,6 @@ def cars():
     
 def main():
     screenSize(1200,900)
-    # setBackgroundImage('racetrack.png')
     cars()
    
 
@@ -114,5 +129,4 @@ def main():
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
-    # call the main function
     main()
--- tutorial.py
+++ tutorial.py
@@ -13,49 +13,130 @@ white = (255,255,255)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
-grass = pygame.image.load('grass.png')
+grass = pygame.image.load('red.png')
 road = pygame.image.load('road.png')
+finish = pygame.image.load('finish_line.png')
+check = pygame.image.load('blue.png')
 gameDisplay = pygame.display.set_mode((dis_width,dis_height))
 class Tile:
 
-    def __init__(self,slow_down):
+    def __init__(self,slow_down,finish_line,block_path,checkpoint):
         self.slow_down = slow_down
-
+        self.finish_line = finish_line
+        self.block_path = block_path
+        self.checkpoint = checkpoint
 def create_map():
-    mymap = [[ Tile(False) for x in range(0,map_width)] for y in range(0,map_height)]
+    mymap = [[ Tile(False,False,False,(False,0)) for x in range(0,map_width)] for y in range(0,map_height)]
     #map2 = [[ Tile(False) for x in range(0,map_width)] for y in range(0,map_height)]
 
 #mymap = [[r*30],[r*30]]
 
 #mymap[10][10].slow_down = True
-    for y in range(10,36):
-        for x in range(10,51):
+    for y in range(12,33):
+        for x in range(15,46):
             mymap[y][x].slow_down = True
+    mymap[12][15].slow_down = False
+    mymap[32][45].slow_down = False
+    mymap[12][45].slow_down = False
+    for i in range(3):
+        mymap[32 - i][15].slow_down = False
+        mymap[12+i][15].slow_down = False
+        mymap[32-i][45].slow_down = False
+        mymap[12+i][45].slow_down = False
+    for i in range(3):
+        mymap[32][15+i].slow_down = False
+        mymap[12][15+i].slow_down = False
+        mymap[32][45-i].slow_down = False
+        mymap[12][45-i].slow_down = False
+    for i in range(60):
+        
+        mymap[0][i].slow_down = True
+        mymap[1][i].slow_down = True
+        mymap[44][i].slow_down = True
+        mymap[43][i].slow_down = True
+        if i < 45:
+            mymap[i][0].slow_down = True
+            mymap[i][1].slow_down = True
+            mymap[i][59].slow_down = True
+            mymap[i][58].slow_down = True
+            
 
-    mymap[0][0].slow_down = True
     #top left
-    mymap[44][59].slow_down = True
+    for i in range(8):
+        mymap[(map_height-1)-i][map_width - 2].slow_down = True
+        mymap[1][i].slow_down = True
+        mymap[(map_height-1)-i][1].slow_down = True
+        mymap[1][(map_width-1)-i].slow_down = True
+        if i < 7:
+            mymap[(map_height-1)-i][map_width - 3].slow_down = True
+            mymap[2][i].slow_down = True
+            mymap[(map_height-1)-i][2].slow_down = True
+            mymap[2][(map_width-1)-i].slow_down = True
+        if i < 6:
+            mymap[(map_height-1)-i][map_width - 4].slow_down = True
+            mymap[3][i].slow_down = True
+            mymap[(map_height-1)-i][3].slow_down = True
+            mymap[3][(map_width-1)-i].slow_down = True
+        if i < 5:
+            mymap[(map_height-1)-i][map_width - 4].slow_down = True
+            mymap[4][i].slow_down = True
+            mymap[(map_height-1)-i][4].slow_down = True
+            mymap[4][(map_width-1)-i].slow_down = True
+        if i < 4:
+            mymap[(map_height-1)-i][map_width - 5].slow_down = True
+            mymap[5][i].slow_down = True
+            mymap[(map_height-1)-i][5].slow_down = True
+            mymap[5][(map_width-1)-i].slow_down = True
+        if i < 3:
+            mymap[(map_height-1)-i][map_width - 6].slow_down = True
+            mymap[6][i].slow_down = True
+            mymap[(map_height-1)-i][6].slow_down = True
+            mymap[6][(map_width-1)-i].slow_down = True
+        if i < 2:
+            mymap[(map_height-1)-i][map_width - 7].slow_down = True
+            mymap[7][i].slow_down = True
+            mymap[(map_height-1)-i][7].slow_down = True
+            mymap[7][(map_width-1)-i].slow_down = True
+
+    
+    
+    
+    
     #bottom right
     mymap[44][0].slow_down = True
     #bottom left
     mymap[0][59].slow_down = True
+    for y in range(3):
+        for i in range(2,15):
+            if i < 12:
+                mymap[i][29+y].finish_line = True
+                mymap[i+31][29+y].checkpoint = (True,2)
+            mymap[22+y][i].checkpoint = (True,3)
+            mymap[22+y][i+43].checkpoint = (True,1)
+        mymap[22+y][45].checkpoint = (False,1)
+
     #top right
     return mymap
-
-
-
 #surface main^
 def draw_map(m):
     for x in range(map_width):
         for y in range(map_height):
+            if m[y][x].slow_down == False:
+                #draw road
+                gameDisplay.blit(road, (x*cell_width, y* cell_height))
             if m[y][x].slow_down:
                 #draw grass
                 gameDisplay.blit(grass, (x*cell_width, y* cell_height))
 
+            if m[y][x].finish_line:
+                #draw finish line
+                gameDisplay.blit(finish, (x*cell_width, y* cell_height))
+            if m[y][x].checkpoint[0]:
+                gameDisplay.blit(check, (x*cell_width, y* cell_height))
+
+
                 
-            else:
-                #draw road
-                gameDisplay.blit(road, (x*cell_width, y* cell_height))
+    
                 
 
 
